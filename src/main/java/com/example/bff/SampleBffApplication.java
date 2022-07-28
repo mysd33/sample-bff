823c8c07e6247b675408f56376bc5884171aea9c
mysd33 <mysd.33@gmail.com>
2022-07-28 21:12:10 +0900

    新規作成

---
+++ src/main/java/com/example/bff/SampleBffApplication.java
@@ -0,0 +1,13 @@
+package com.example.bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleBffApplication.class, args);
+	}
+
+}
