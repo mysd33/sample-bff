4170a3494f6b3c3ee4274b4174f4f7a1e7bc657d
mysd33 <mysd.33@gmail.com>
2022-12-17 12:15:43 +0900

    @Transactional付け忘れ

--- src/main/java/com/example/bff/domain/service/LoginUserDetailService.java
+++ src/main/java/com/example/bff/domain/service/LoginUserDetailService.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.bff.domain.model.LoginUserDetails;
 import com.example.bff.domain.model.User;
@@ -14,11 +15,13 @@ import lombok.RequiredArgsConstructor;
 /**
  * JDBCによる認証処理用UserDetailService実装クラス 
  */
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class LoginUserDetailService implements UserDetailsService {
 	private final UserRepository repository;
 	
+	@Transactional(readOnly = true)
 	@Override
 	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
 		User user = repository.findOne(userId);
