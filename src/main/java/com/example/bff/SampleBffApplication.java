675512542dace3c0528aaee9182d5c96fa4a937c
mysd33 <mysd.33@gmail.com>
2022-12-30 15:23:26 +0900

    コード削除

--- src/main/java/com/example/bff/SampleBffApplication.java
+++ src/main/java/com/example/bff/SampleBffApplication.java
@@ -3,15 +3,12 @@ package com.example.bff;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.amazonaws.xray.AWSXRay;
-
 
 
 @SpringBootApplication
 public class SampleBffApplication {
 
-	public static void main(String[] args) {
-		AWSXRay.beginSegment("sample-bff");
+	public static void main(String[] args) {		
 		SpringApplication.run(SampleBffApplication.class, args);
 	}
 
