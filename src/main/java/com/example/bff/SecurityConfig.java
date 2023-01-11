8ed4d5751bccab14fd32d2ccede44c644151f507
mysd33 <mysd.33@gmail.com>
2023-01-11 19:13:49 +0900

    SecurityConfigの本格対処

--- src/main/java/com/example/bff/SecurityConfig.java
+++ src/main/java/com/example/bff/SecurityConfig.java
@@ -1,9 +1,10 @@
 package com.example.bff;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,13 +20,9 @@ public class SecurityConfig {
 	// https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
 	// https://www.docswell.com/s/MasatoshiTada/KGVY9K-spring-security-intro
 
-	//@Profile("dev")
-	@Bean
-	public WebSecurityCustomizer webSecurityCustomizer() {
-		return (web) -> web.ignoring().antMatchers("/h2-console/**") // h2-consoleにアクセス許可				
-				.antMatchers("/api/**"); // REST APIにアクセス許可
-	}
-
+	/**
+	 * Spring Securityによる認証認可設定
+	 */
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		// フォーム認証によるログイン処理
@@ -36,31 +33,45 @@ public class SecurityConfig {
 				.passwordParameter("password") // ログインページのパスワード
 				.defaultSuccessUrl("/menu", true) // ログイン成功後の遷移先
 				.permitAll())
-			// ログアウト処理
-			.logout(logout -> logout.logoutUrl("/logout") // ログアウトのURL
-				.logoutSuccessUrl("/")) // ログアウト成功後のURL
-			// 認可設定
-			.authorizeHttpRequests(authz -> authz
-					.antMatchers("/webjars/**").permitAll() // webjarsへアクセス許可
-					.antMatchers("/css/**").permitAll()// cssへアクセス許可
-					.antMatchers("/js/**").permitAll()// jsへアクセス許可
-					.antMatchers("/login").permitAll() // ログインページは直リンクOK
-					.antMatchers("/actuator/**").permitAll() // actuatorのAPIへアクセス許可
-					.antMatchers("/v3/api-docs/**").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
-					.antMatchers("/v3/api-docs*").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
-					.antMatchers("/swagger-ui/**").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
-					.antMatchers("/swagger-ui.html").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
-					// .antMatchers("/api/**").permitAll()// REST APIへアクセス許可
-					// .antMatchers("/h2-console/**").permitAll() // H2 Consoleへのアクセス許可
-					.antMatchers("/admin").hasAuthority("ROLE_ADMIN") // ユーザ管理画面は管理者ユーザーのみ許可
-					.antMatchers("/user*").hasAuthority("ROLE_ADMIN") // ユーザ管理画面は管理者ユーザーのみ許可
-					.anyRequest().authenticated() // それ以外は認証・認可が必要
-		);
-		
-		// H2 Console向け設定
-		// http.csrf().ignoringAntMatchers("/h2-console/**");
-		// http.antMatcher("/h2-console/**").headers().frameOptions().disable();
+				// ログアウト処理
+				.logout(logout -> logout.logoutUrl("/logout") // ログアウトのURL
+						.logoutSuccessUrl("/")) // ログアウト成功後のURL
+				// 認可設定
+				.authorizeHttpRequests(authz -> authz.antMatchers("/webjars/**").permitAll() // webjarsへアクセス許可
+						.antMatchers("/css/**").permitAll()// cssへアクセス許可
+						.antMatchers("/js/**").permitAll()// jsへアクセス許可
+						.antMatchers("/login").permitAll() // ログインページは直リンクOK
+						.antMatchers("/actuator/**").permitAll() // actuatorのAPIへアクセス許可
+						.antMatchers("/v3/api-docs/**").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
+						.antMatchers("/v3/api-docs*").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
+						.antMatchers("/swagger-ui/**").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
+						.antMatchers("/swagger-ui.html").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
+						.antMatchers("/api/**").permitAll()// REST APIへアクセス許可
+						.antMatchers("/admin").hasAuthority("ROLE_ADMIN") // ユーザ管理画面は管理者ユーザーのみ許可
+						.antMatchers("/user*").hasAuthority("ROLE_ADMIN") // ユーザ管理画面は管理者ユーザーのみ許可
+						.anyRequest().authenticated() // それ以外は認証・認可が必要
+				)
+				// REST APIはCSRF保護不要
+				.csrf().ignoringAntMatchers("/api/**");
+		return http.build();
+	}
 
+	/**
+	 * H2 Consoleのアクセス許可対応
+	 */
+	@Profile("dev")
+	@Order(1)
+	@Bean
+	public SecurityFilterChain securityFilterChainForH2Console(HttpSecurity http) throws Exception {
+		//H2 ConsoleのURLに対して
+		http.antMatcher("/h2-console/**")
+			.authorizeHttpRequests(
+				// 認証不要でアクセス許可
+				authz -> authz.anyRequest().permitAll())
+			// CSRF保護不要			
+			.csrf().disable()
+			// H2 Consoleの表示ではframeタグを使用しているのでX-FrameOptionsを無効化
+			.headers().frameOptions().disable();
 		return http.build();
 	}
 
