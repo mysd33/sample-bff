7da40206397d4010ee647caf82afd2b981e92871
mysd33 <mysd.33@gmail.com>
2023-02-12 16:00:44 +0900

    S3rverの場合のchunk-signatureの対処策

--- src/main/java/com/example/fw/common/objectstorage/config/S3LocalS3rverFakeConfig.java
+++ src/main/java/com/example/fw/common/objectstorage/config/S3LocalS3rverFakeConfig.java
@@ -16,44 +16,49 @@ import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
 
 /**
  * 
  * S3が開発環境上でのローカルサーバFake（s3rver）実行に置き換える設定クラス
  *
  */
-@Profile("dev")  
+@Profile("dev")
 @ConditionalOnProperty(prefix = "aws.s3.localfake", name = "type", havingValue = "s3rver")
 @Configuration
-public class S3LocalS3rverFakeConfig {    
+public class S3LocalS3rverFakeConfig {
     @Value("${aws.s3.region:ap-northeast-1}")
     private String region;
     @Value("${aws.s3.bucket}")
     private String bucket;
     @Value("${aws.s3.port:4568}")
     private String port;
-      
+
     /**
      * オブジェクトストレージアクセスクラス
-     */        
+     */
     @Bean
     public ObjectStorageFileAccessor objectStorageFileAccessor(S3Client s3Client) {
         return new S3ObjectStorageFileAccessor(s3Client, bucket);
-    } 
-    
+    }
+
     /**
      * S3クライアント
      */
     @Bean
     public S3Client s3Client() {
-        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(
-                "S3RVER",
-                "S3RVER");
+        // ダミーのクレデンシャル
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create("S3RVER", "S3RVER");
+        // @formatter:off
         return S3Client.builder()                
                 .region(Region.of(region))       
                 .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .endpointOverride(URI.create("http://localhost:" + port))
+                //S3rverの場合、putしたファイルにchunk-signatureが入ってしまうため対処策として設定
+                .serviceConfiguration(S3Configuration.builder()
+                        .chunkedEncodingEnabled(false).build())
                 .build();        
+        // @formatter:on
     }
 
     /**
@@ -62,9 +67,7 @@ public class S3LocalS3rverFakeConfig {
      */
     @Bean
     public BucketCreateInitializer bucketCreateInitializer() {
-        return new BucketCreateInitializer(s3Client(), bucket);       
+        return new BucketCreateInitializer(s3Client(), bucket);
     }
-    
-
 
 }
