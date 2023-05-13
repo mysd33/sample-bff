c46903ebb802387e3cd79ec6acd7a9cc40bf6343
mysd33 <mysd.33@gmail.com>
2023-05-13 17:26:15 +0900

    SpringBoot3対応の微修正

--- README.md
+++ README.md
@@ -207,9 +207,9 @@ postgres> CREATE DATABASE testdb;
 * Mavenビルド
 ```sh
 #Windows
-.\mvnw.cmd package
+.\mvnw.cmd package -P production
 #Linux/Mac
-./mvnw package
+./mvnw package -P production
 ```
 * ローカルでDockerビルド
 ```sh
@@ -255,7 +255,6 @@ docker push XXXXXXXXXXXX.dkr.ecr.ap-northeast-1.amazonaws.com/sample-bff:latest
 | | セッション管理 | 通常、SpringMVCのセッション管理機能で管理するが、オートスケーリング等の対応のため、APサーバ上で保持していたセッション情報をRedisサーバ（AWSの場合、ElastiCache for Redis）に外部化するためにSpring Session Data Redisを利用する。 | - | - |
 | | 集約例外ハンドリング | SpringMVCのControllerAdviceやAOPを利用し、エラー（例外）発生時、エラーログの出力、DBのロールバック、エラー画面やエラー電文の返却といった共通的なエラーハンドリングを実施する。 | ○ | com.example.fw.web.advice、com.example.fw.web.aspect |
 | | トランザクション管理機能 | Spring Frameworkのトランザクション管理機能を利用して、@Transactionalアノテーションによる宣言的トランザクションを実現する機能を提供する。 | - | - |
-| | 分散トレーシング | Spring Cloud Sleathを利用して、トレースIDやスパンIDをAP間でのREST API呼び出しで引継ぎログに記録することで、分散トレーシングを実現する。 | - | - |
 | | 分散トレーシング（X-Ray） | AWS X-Rayを利用して、サービス間の分散トレーシング・可視化を実現する。 | ○ | com.example.fw.web.aspect<br/>com.example.fw.servlet<br/>com.example.fw.common.async<br/>com.example.fw.common.dynamodb<br/>com.example.fw.common.httpclient<br/>com.example.fw.common.objectstorage |
 | | ヘルスチェック | Spring Boot Actuatorを利用して、ヘルスチェックエンドポイントを提供する。その他、Micrometerメトリックの情報提供も行う。 | - | - |
 | | グレースフルシャットダウン | SpringBootの機能で、Webサーバ（組み込みTomcat）のグレースフルシャットダウン機能を提供する 。 | - | - |
@@ -264,6 +263,7 @@ docker push XXXXXXXXXXXX.dkr.ecr.ap-northeast-1.amazonaws.com/sample-bff:latest
 | | オブジェクトストレージ（S3）アクセス | AWS SDK for Java 2.xのS3クライアント（S3Client)を使って、S3のアクセス機能を提供する。開発時にS3アクセスできない場合を考慮して通常のファイルシステムへのFakeに切り替える。 | ○ | com.example.fw.common.objectstorage |
 | | HTTPクライアント | WebClientやRestTemplateを利用してREST APIの呼び出しやサーバエラー時の例外の取り扱いを制御する。 | ○ | com.example.fw.common.httpclient |
 | | リトライ・サーキットブレーカ | Spring Cloud Circuit Breaker（Resillience4j）を利用し、REST APIの呼び出しでの一時的な障害に対する遮断やフォールバック処理等を制御する。また、WebClientのリトライ機能でエクスポネンシャルバックオフによりリトライを実現する。なお、AWSリソースのAPI呼び出しは、AWS SDKにてエクスポネンシャルバックオフによりリトライ処理を提供。 | - | - |
+| | 分散トレーシング | Micrometer Tracingを利用して、トレースIDやスパンIDをAP間でのREST API呼び出しで引継ぎログに記録することで、分散トレーシングを実現する。 | - | - |
 | | 非同期実行依頼 | Spring JMS、Amazon SQS Java Messaging Libraryを利用し、SQSの標準キューを介した非同期実行依頼のメッセージを送信する。 | ○ | com.example.fw.common.async |
 | | 入力チェック| Java BeanValidationとSpringのValidation機能を利用し、単項目チェックや相関項目チェックといった画面の入力項目に対する形式的なチェックを実施する。 | ○ | com.example.fw.common.validation<br/>com.example.fw.web.validation |
 | | メッセージ管理 | MessageResourceで画面やログに出力するメッセージを管理する。 | ○ | com.example.fw.common.message |
--- src/main/java/com/example/bff/SecurityConfig.java
+++ src/main/java/com/example/bff/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.example.bff;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -27,7 +29,7 @@ public class SecurityConfig {
     // Spring Securityのデバッグモード
     @Value("${spring.websecurity.debug:false}")
     boolean webSecurityDebug;
-    
+
     /**
      * Spring Securityのデバッグモードの設定
      * 
@@ -36,42 +38,40 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return web -> web.debug(webSecurityDebug);
     }
-    
+
     /**
      * Spring Securityによる認証認可設定
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        // @formatter:off
-		// フォーム認証によるログイン処理
-		http.formLogin(login -> login.loginProcessingUrl("/authenticate") // ログイン処理のパス
-				.loginPage("/login") // ログインページの指定
-				.failureUrl("/login?error") // ログイン失敗時の遷移先
-				.usernameParameter("userId") // ログインページのユーザーID
-				.passwordParameter("password") // ログインページのパスワード
-				.defaultSuccessUrl("/menu", true) // ログイン成功後の遷移先
-				.permitAll())
-				// ログアウト処理
-				.logout(logout -> logout.logoutUrl("/logout") // ログアウトのURL
-						.logoutSuccessUrl("/")) // ログアウト成功後のURL
-				// 認可設定
-				.authorizeHttpRequests(authz -> authz.requestMatchers("/webjars/**").permitAll() // webjarsへアクセス許可
-						.requestMatchers("/css/**").permitAll()// cssへアクセス許可
-						.requestMatchers("/js/**").permitAll()// jsへアクセス許可
-						.requestMatchers("/login").permitAll() // ログインページは直リンクOK
-						.requestMatchers("/actuator/**").permitAll() // actuatorのAPIへアクセス許可
-						.requestMatchers("/v3/api-docs/**").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
-						.requestMatchers("/v3/api-docs*").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
-						.requestMatchers("/swagger-ui/**").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
-						.requestMatchers("/swagger-ui.html").permitAll() // Springdoc-openapiのドキュメントへのアクセス許可
-						.requestMatchers("/api/**").permitAll()// REST APIへアクセス許可
-						.requestMatchers("/admin").hasAuthority("ROLE_ADMIN") // ユーザ管理画面は管理者ユーザーのみ許可
-						.requestMatchers("/user*").hasAuthority("ROLE_ADMIN") // ユーザ管理画面は管理者ユーザーのみ許可
-						.anyRequest().authenticated() // それ以外は認証・認可が必要
-				)
-				// REST APIはCSRF保護不要
-				.csrf().ignoringRequestMatchers("/api/**");
-		// @formatter:on
+        // フォーム認証にによるログイン処理
+        http.formLogin(login -> login.loginProcessingUrl("/authenticate") // ログイン処理のパス
+                .loginPage("/login") // ログインページの指定
+                .failureUrl("/login?error") // ログイン失敗時の遷移先
+                .usernameParameter("userId") // ログインページのユーザーID
+                .passwordParameter("password") // ログインページのパスワード
+                .defaultSuccessUrl("/menu", true) // ログイン成功後の遷移先
+                .permitAll())
+                // ログアウト処理
+                .logout(logout -> logout.logoutUrl("/logout") // ログアウトのURL
+                        .logoutSuccessUrl("/")) // ログアウト成功後のURL
+                // 認可設定
+                .authorizeHttpRequests(authz -> authz.requestMatchers("/webjars/**").permitAll() // webjarsへアクセス許可
+                        .requestMatchers("/css/**").permitAll()// cssへ認証なしでアクセス許可
+                        .requestMatchers("/js/**").permitAll()// jsへ認証なしでアクセス許可
+                        .requestMatchers("/login").permitAll() // ログインページへ認証なしでアクセス許可
+                        .requestMatchers("/actuator/**").permitAll() // actuatorのAPIへ認証なしでアクセス許可
+                        .requestMatchers("/v3/api-docs/**").permitAll() // Springdoc-openapiのドキュメント認証なしでアクセス許可
+                        .requestMatchers("/v3/api-docs*").permitAll() // Springdoc-openapiのドキュメントへ認証なしでアクセス許可
+                        .requestMatchers("/swagger-ui/**").permitAll() // Springdoc-openapiのドキュメントへ認証なしでアクセス許可
+                        .requestMatchers("/swagger-ui.html").permitAll() // Springdoc-openapiのドキュメントへ認証なしでアクセス許可
+                        .requestMatchers("/api/**").permitAll()// REST APIへアクセス許可
+                        .requestMatchers("/admin").hasAuthority("ROLE_ADMIN") // ユーザ管理画面は管理者ユーザのみ許可
+                        .requestMatchers("/user*").hasAuthority("ROLE_ADMIN") // ユーザ管理画面は管理者ユーザのみ許可
+                        .anyRequest().authenticated() // それ以外は認証が必要
+                )
+                // REST APIはCSRF保護不要
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"));
         return http.build();
     }
 
@@ -82,17 +82,15 @@ public class SecurityConfig {
     @Order(1)
     @Bean
     public SecurityFilterChain securityFilterChainForH2Console(HttpSecurity http) throws Exception {
-        // @formatter:off
-		//H2 ConsoleのURLに対して
-		http.securityMatcher("/h2-console/**")
-			.authorizeHttpRequests(
-				// 認証不要でアクセス許可
-				authz -> authz.anyRequest().permitAll())
-			// CSRF保護不要			
-			.csrf().disable()
-			// H2 Consoleの表示ではframeタグを使用しているのでX-FrameOptionsを無効化
-			.headers().frameOptions().disable();
-        // @formatter:on		
+        // H2 ConsoleのURLに対して
+        http.securityMatcher(toH2Console())//
+                .authorizeHttpRequests(
+                        // 認証不要でアクセス許可
+                        authz -> authz.anyRequest().permitAll())
+                // CSRF保護不要
+                .csrf(csrf -> csrf.disable())
+                // H2 Consoleの表示ではframeタグを使用しているのでX-FrameOptionsを無効化
+                .headers(headers -> headers.frameOptions().disable());
         return http.build();
     }
 
