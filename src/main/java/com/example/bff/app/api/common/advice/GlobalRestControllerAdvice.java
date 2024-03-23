edf95cf39985975193fca32c86a63689732a5e45
mysd33 <mysd.33@gmail.com>
2024-03-23 13:57:31 +0900

    システムエラー時の画面表示のための対処

--- src/main/java/com/example/bff/app/api/common/advice/GlobalRestControllerAdvice.java
+++ src/main/java/com/example/bff/app/api/common/advice/GlobalRestControllerAdvice.java
@@ -2,6 +2,7 @@ package com.example.bff.app.api.common.advice;
 
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.example.bff.app.api.APIPackage;
 import com.example.fw.web.advice.AbstractRestControllerAdvice;
 import com.example.fw.web.advice.ErrorResponseCreator;
 
@@ -10,7 +11,7 @@ import com.example.fw.web.advice.ErrorResponseCreator;
  * 集約例外ハンドリングのためのRestControllerAdviceクラス
  *
  */
-@RestControllerAdvice
+@RestControllerAdvice(basePackageClasses = { APIPackage.class })
 public class GlobalRestControllerAdvice extends AbstractRestControllerAdvice {
 
     public GlobalRestControllerAdvice(ErrorResponseCreator errorResponseCreator) {
