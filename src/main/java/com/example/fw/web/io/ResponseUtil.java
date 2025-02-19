46104a326e5713ceeb268b195ad4852dce37c444
mysd33 <mysd.33@gmail.com>
2025-02-20 07:31:13 +0900

    ResponseUtilのリファクタリング

--- src/main/java/com/example/fw/web/io/ResponseUtil.java
+++ src/main/java/com/example/fw/web/io/ResponseUtil.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.InputStream;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
 
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
@@ -23,48 +24,66 @@ public class ResponseUtil {
     /**
      * PDFファイル用のレスポンスを生成する（InputStream版）
      * 
-     * @param inputStream 帳票の入力ストリーム
+     * @param inputStream ダウンロードするPDFファイルの入力ストリーム
      * @param fileName    ファイル名
      * @fileSize ファイルサイズ
      * @return レスポンスデータ
      */
     public static ResponseEntity<Resource> createResponseForPDF(final InputStream inputStream, final String fileName,
             final long fileSize) {
+        // CustomInputStreamResourceを使用
         Resource reportResource = new CustomInputStreamResource(inputStream, fileSize);
-        return ResponseEntity.ok()//
-                // .contentType(MediaType.APPLICATION_OCTET_STREAM)//
-                .contentType(MediaType.APPLICATION_PDF)//
-                .contentLength(fileSize)//
-                .cacheControl(CacheControl.noCache())//
-                .header(HttpHeaders.CONTENT_DISPOSITION, //
-                        ContentDisposition.attachment()//
-                                .filename(encodeUtf8(fileName))//
-                                .build().toString())//
-                .body(reportResource);
+        return createResponse(MediaType.APPLICATION_PDF, reportResource, fileName);
     }
 
     /**
      * PDFファイル用のレスポンスを生成する（File版）
      * 
-     * @param file 帳票ファイル
+     * @param file ダウンロードするPDFファイル
      * @return レスポンスデータ
      */
     public static ResponseEntity<Resource> createResponseForPDF(final File file) {
         // FileSystemResourceを使用
         Resource reportResource = new FileSystemResource(file);
+        return createResponse(MediaType.APPLICATION_PDF, reportResource, file.getName());
+    }
+
+    /**
+     * PDFファイル用のレスポンスを生成する（Path版）
+     * 
+     * @param file ダウンロードするPDFファイルのパス
+     * @return レスポンスデータ
+     */
+    public static ResponseEntity<Resource> createResponseForPDF(final Path file) {
+        // FileSystemResourceを使用
+        Resource reportResource = new FileSystemResource(file);
+        return createResponse(MediaType.APPLICATION_PDF, reportResource, file.getFileName().toString());
+    }
+
+    /**
+     * ダウンロードファイル用のレスポンスを生成する
+     * 
+     * @param mediaType メディアタイプ
+     * @param resource  ダウンロードするファイルのリソース
+     * @param fileName  ファイル名
+     * @fileSize ファイルサイズ
+     * @return レスポンスデータ
+     */
+    private static ResponseEntity<Resource> createResponse(final MediaType mediaType, final Resource resource,
+            final String fileName) {
         return ResponseEntity.ok()//
-                // .contentType(MediaType.APPLICATION_OCTET_STREAM)//
-                .contentType(MediaType.APPLICATION_PDF)//
+                .contentType(mediaType)//
                 .cacheControl(CacheControl.noCache())//
                 .header(HttpHeaders.CONTENT_DISPOSITION, //
                         ContentDisposition.attachment()//
-                                .filename(file.getName())//
+                                .filename(encodeUtf8(fileName))//
                                 .build().toString())//
-                .body(reportResource);
+                .body(resource);
     }
 
     /**
      * ファイル名をUTF-8でエンコードする
+     * 
      * 
      * @param filename ファイル名
      * @return エンコードされたファイル名
