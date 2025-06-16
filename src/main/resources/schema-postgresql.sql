ff358c7cd4a9f4ad98b0746ae9ecca25968c6a85
mysd33 <mysd.33@gmail.com>
2025-06-16 22:58:13 +0900

    PostgreSQLのデータ長を元に戻す

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS m_user (
     user_id VARCHAR(50) PRIMARY KEY,
     password VARCHAR(100),
-    user_name VARCHAR(100),
+    user_name VARCHAR(50),
     birthday DATE,
     role VARCHAR(50)
 );
