6ad96fdf7cf0f695de1d508e61f91e2936adb0fc
mysd33 <mysd.33@gmail.com>
2025-07-19 00:21:05 +0900

    LogAspect改善

--- src/main/java/com/example/fw/web/aspect/LogAspect.java
+++ src/main/java/com/example/fw/web/aspect/LogAspect.java
@@ -37,28 +37,34 @@ public class LogAspect {
 
     @Around("@within(org.springframework.web.bind.annotation.RestController)")
     public Object aroundRestControllerLog(final ProceedingJoinPoint jp) throws Throwable {
+        // ログ解析を容易にするため、処理開始日時をログ出力するために取得
         LocalDateTime startDateTime = systemDate.now();
+        // 処理時間を計測しログ出力
+        long startTime = System.nanoTime();
         appLogger.info(WebFrameworkMessageIds.I_ON_FW_0006, jp.getSignature(), startDateTime);
         try {
             return jp.proceed();
         } finally {
             // 処理時間を計測しログ出力
-            LocalDateTime endDateTime = systemDate.now();
-            double elapsedTime = SystemDateUtils.calcElapsedTimeByMilliSeconds(startDateTime, endDateTime);
+            long endTime = System.nanoTime();
+            double elapsedTime = SystemDateUtils.calcElapsedTimeByMilliSeconds(startTime, endTime);
             appLogger.info(WebFrameworkMessageIds.I_ON_FW_0007, jp.getSignature(), elapsedTime, startDateTime);
         }
     }
 
     @Around("@within(org.springframework.stereotype.Controller)")
     public Object aroundControllerLog(final ProceedingJoinPoint jp) throws Throwable {
+        // ログ解析を容易にするため、処理開始日時をログ出力するために取得
         LocalDateTime startDateTime = systemDate.now();
+        // 処理時間を計測しログ出力
+        long startTime = System.nanoTime();
         appLogger.info(WebFrameworkMessageIds.I_ON_FW_0004, jp.getSignature(), startDateTime);
         try {
             return jp.proceed();
         } finally {
             // 処理時間を計測しログ出力
-            LocalDateTime endDateTime = systemDate.now();
-            double elapsedTime = SystemDateUtils.calcElapsedTimeByMilliSeconds(startDateTime, endDateTime);
+            long endTime = System.nanoTime();
+            double elapsedTime = SystemDateUtils.calcElapsedTimeByMilliSeconds(startTime, endTime);
             appLogger.info(WebFrameworkMessageIds.I_ON_FW_0005, jp.getSignature(), elapsedTime, startDateTime);
         }
     }
