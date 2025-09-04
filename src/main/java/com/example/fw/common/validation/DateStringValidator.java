0703ad47fd95a28c46e0dc2e20c9c61c1b330b8a
mysd33 <mysd.33@gmail.com>
2025-09-04 19:00:01 +0900

    日付文字列チェックの不具合修正

--- src/main/java/com/example/fw/common/validation/DateStringValidator.java
+++ src/main/java/com/example/fw/common/validation/DateStringValidator.java
@@ -1,5 +1,6 @@
 package com.example.fw.common.validation;
 
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.List;
@@ -15,12 +16,11 @@ import jakarta.validation.ConstraintValidatorContext;
 public class DateStringValidator implements ConstraintValidator<DateString, CharSequence> {
     // 許容する日付のリストを定義
     private static final List<DateTimeFormatter> DATE_FORMATTERS = List.of(//
-            DateTimeFormatter.ofPattern("yyyy-MM-dd"), //
-            DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"), //
-            DateTimeFormatter.ofPattern("yyyy/MM/dd"), //
-            DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-    // 日付文字列としての最小長
-    private static final int MIN_LENGTH = 10;
+            DateTimeFormatter.ofPattern("uuuu-MM-dd").withResolverStyle(java.time.format.ResolverStyle.STRICT) //
+    // 必要に応じて他のフォーマットも追加
+    );
+    // 日付文字列としての文字列長
+    private static final int DATE_STRING_LENGTH = 10;
 
     @Override
     public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
@@ -29,14 +29,14 @@ public class DateStringValidator implements ConstraintValidator<DateString, Char
         if (value == null || value.toString().isBlank()) { // CharSequenceがtoString()を実装している前提
             return true;
         }
-        // 最低文字列長の場合は日付形式でないと判断し、falseを返す        
-        if (value.length() < MIN_LENGTH) {
+        // 文字列長が合致しない場合日付形式でないと判断し、falseを返す
+        if (value.length() != DATE_STRING_LENGTH) {
             return false;
         }
         for (DateTimeFormatter formatter : DATE_FORMATTERS) {
             try {
                 // 日付形式に変換できるかどうかを検証
-                formatter.parse(value);
+                LocalDate.parse(value, formatter);
                 return true;
             } catch (DateTimeParseException e) {
                 // 変換できない場合は次のフォーマットを試すため何もしない
