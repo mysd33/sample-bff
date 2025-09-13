50f1eecec974d0178212181957d9b03b3ca43057
mysd33 <mysd.33@gmail.com>
2025-09-13 09:41:22 +0900

    ロギング拡張機能の修正

--- src/main/java/com/example/bff/InfraConfig.java
+++ src/main/java/com/example/bff/InfraConfig.java
@@ -16,7 +16,7 @@ import com.example.fw.common.async.repository.JobRequestRepository;
 import com.example.fw.common.async.repository.JobRequestRepositoryImpl;
 import com.example.fw.common.db.config.DynamicRoutingDataSourceConfig;
 import com.example.fw.common.httpclient.config.WebClientConfigPackage;
-import com.example.fw.common.logging.config.LoggingExtensionConfig;
+import com.example.fw.common.logging.config.LoggingConfigPackage;
 import com.example.fw.common.metrics.config.MetricsConfig;
 import com.example.fw.common.objectstorage.config.S3ConfigPackage;
 import com.example.fw.common.reports.config.ReportsConfigPackage;
@@ -28,17 +28,17 @@ import com.example.fw.web.token.TransactionTokenPackage;
  *
  */
 @Configuration
+// RESTクライアント、オブジェクトストレージアクセス、非同期処理依頼の設定、
+// 帳票出力の設定、ロギング拡張機能の設定を追加
 @ComponentScan(basePackageClasses = { WebClientConfigPackage.class, S3ConfigPackage.class, //
-        SQSCommonConfigPackage.class, ReportsConfigPackage.class }) // , //
+        SQSCommonConfigPackage.class, ReportsConfigPackage.class, LoggingConfigPackage.class }) //
 // 鍵管理機能とPDF電子署名機能を有効化するには、ComponentScanに以下を追加する
-// KeyManagementConfigPackage.class, DigitalSignatureConfigPackage.class })
+// , KeyManagementConfigPackage.class, DigitalSignatureConfigPackage.class })
+//動的ルーティングによるデータソース設定、Micrometerのカスタムメトリックス設定を追加
+@Import({ DynamicRoutingDataSourceConfig.class, MetricsConfig.class })
 // トランザクショントークンチェックのMyBatisのMapperをスキャンさせるために、業務APのMapper含めて明示的にスキャンする設定を追加
 @MapperScan(basePackageClasses = { TransactionTokenPackage.class,
         RepositoryPackage.class }, annotationClass = Mapper.class)
-// 動的ルーティングによるデータソース設定を追加
-// Micrometerのカスタムメトリックス設定を追加
-// ロギング拡張機能の設定を追加
-@Import({ DynamicRoutingDataSourceConfig.class, MetricsConfig.class, LoggingExtensionConfig.class })
 public class InfraConfig {
     @Value("${delayed.batch.queue}")
     private String queueName;
