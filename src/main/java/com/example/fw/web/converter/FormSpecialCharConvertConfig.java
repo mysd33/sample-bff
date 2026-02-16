d1c497151156475930671210c8280cede932e418
mysd33 <mysd.33@gmail.com>
2026-02-16 22:02:31 +0900

    ソースコードコメント修正

--- src/main/java/com/example/fw/web/converter/FormSpecialCharConvertConfig.java
+++ src/main/java/com/example/fw/web/converter/FormSpecialCharConvertConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
- * 特殊文字のコードポイント変換の設定クラス
+ * Form用の特殊文字のコードポイント変換の設定クラス
  */
 @Configuration
 public class FormSpecialCharConvertConfig {
